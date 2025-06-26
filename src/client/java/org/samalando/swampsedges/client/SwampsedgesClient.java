07a89b39ec5d4dfc5316a5f9187be06a7870175b
Samalando <156577273+Samalando@users.noreply.github.com>
2025-06-26 11:44:14 +0100

    initial commit :D
    creartion of le reporitory

---
+++ src/client/java/org/samalando/swampsedges/client/SwampsedgesClient.java
@@ -0,0 +1,10 @@
+package org.samalando.swampsedges.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SwampsedgesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/samalando/swampsedges/Swampsedges.java
@@ -0,0 +1,10 @@
+package org.samalando.swampsedges;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Swampsedges implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
