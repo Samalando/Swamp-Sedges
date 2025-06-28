f60a13202f829b47a86a63856faebb135982a3a5
Samalando <156577273+Samalando@users.noreply.github.com>
2025-06-28 09:13:26 +0100

    feat: cattails models, textures and code
    doesnt work patience my child

--- src/main/java/org/samalando/swampsedges/Swampsedges.java
+++ src/main/java/org/samalando/swampsedges/Swampsedges.java
@@ -1,10 +1,12 @@
 package org.samalando.swampsedges;
 
 import net.fabricmc.api.ModInitializer;
+import org.samalando.swampsedges.items.cattail;
 
 public class Swampsedges implements ModInitializer {
-
+public static final String MOD_ID = "swampsedges";
     @Override
     public void onInitialize() {
+        cattail.initialize();
     }
 }
---
+++ src/main/java/org/samalando/swampsedges/items/cattail.java
@@ -0,0 +1,32 @@
+package org.samalando.swampsedges.items;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import org.samalando.swampsedges.Swampsedges;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+
+public class cattail {
+
+
+    public static final Item CATTAIL = register(new Item(new Item.Settings()), "cattail");
+    public static Item register(Item item, String id) {
+        Identifier cattail = Identifier.of(Swampsedges.MOD_ID, id);
+
+        // Register the item.
+        Identifier itemID = null;
+        Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
+
+        // Return the registered item!
+        return registeredItem;
+    }
+
+    public static void initialize() {
+        // Get the event for modifying entries in the ingredients group.
+// And register an event handler that adds our suspicious item to the ingredients group.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL)
+                .register((itemGroup) -> itemGroup.add(cattail.CATTAIL));
+    }
+}
